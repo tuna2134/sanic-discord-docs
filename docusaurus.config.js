19851867f60d645661bbc8c87b215da1e876c0a8
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-03-20 09:36:42 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'sanic-discord',
+  tagline: 'Easy to use discord api.',
+  url: 'https://tuna2134.github.io',
+  baseUrl: '/sanic-discord-docs',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'tuna2134', // Usually your GitHub org/user name.
+  projectName: 'sanic-discord-docs', // Usually your repo name.
 
   presets: [
     [

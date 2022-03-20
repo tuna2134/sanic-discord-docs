ea6e86615a7372d13864d7db6e97e5891a0dcbb8
tuna2134 <masato.04.11.2007@gmail.com>
2022-03-20 09:51:07 +0900

    a

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/rocky/src/docs/node_modules/infima/dist/css/default/default.css'),
+  require('/home/rocky/src/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/rocky/src/docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/home/rocky/src/docs/src/css/custom.css'),
+];

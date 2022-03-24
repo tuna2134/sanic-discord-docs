28d07f320fae4c04e9a67d838b76431b58468e6b
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-03-25 08:24:17 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,7 +105,7 @@ const config = {
               },
               {
                 label: 'PyPI',
-                href: "https://pypi.org/project/sanic-discord
+                href: "https://pypi.org/project/sanic-discord"
               },
             ],
           },
